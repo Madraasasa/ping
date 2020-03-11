87c335bb161b28c47f3b5cd9a82f8faadd08d586
Maruf <36636180+Madraasasa@users.noreply.github.com>
2020-03-11 22:39:03 +0500

    Add files via upload

--- ping.py
+++ ping.py
@@ -2,26 +2,40 @@ import telebot
 import time
 import os
 
+token = os.environ.get('TOKEN', '938006327:AAFMpLy69tCZ8HOfI9HZI_0YouVWsvW21Jw')
+bot = telebot.TeleBot(token=token)
+chat_id = os.environ.get('CHAT_ID', 120929625)
+sec = os.environ.get('SEC', 600)
+host = os.environ.get('HOST', 'google.com')
 
-bot = telebot.TeleBot(token='938006327:AAFMpLy69tCZ8HOfI9HZI_0YouVWsvW21Jw')
-chat_id = 120929625
-sec = 600
 
-def ping(hostname):
-    response = os.system("ping -c 1 " + hostname)
-    if response == 0:
-        # print(hostname, 'is up!')
-        return 0
-    else:
-        # print(hostname, 'is down!')
-        return 1
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
 
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return subprocess.call(command) == 0
 
 while True:
-    a = ping('google.com')
-    print(a)
-    bot.send_message(chat_id=chat_id, text= f'result --{a}')
+    a = ping(host)
+    result = host
+    if a:
+        result += '---- up'
+        print('-----')
+        bot.send_message(chat_id=chat_id, text=result)
+    else:
+        result += '---- down'
+        bot.send_message(chat_id=chat_id, text=result)
     time.sleep(sec)
-
 
 
